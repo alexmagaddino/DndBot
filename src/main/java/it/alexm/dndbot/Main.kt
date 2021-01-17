7ae40acdd7b2f9e2194bb90c0457395b52b3e27f
Alessandro Magaddino <alex.magaddino@outlook.it>
2021-01-17 13:18:46 +0100

    creati i primi test

--- src/main/java/it/alexm/dndbot/Main.kt
+++ src/main/java/it/alexm/dndbot/Main.kt
@@ -1,14 +1,8 @@
 package it.alexm.dndbot
 
-import com.google.gson.Gson
 import com.jessecorbett.diskord.dsl.bot
 import com.jessecorbett.diskord.dsl.command
 import com.jessecorbett.diskord.dsl.commands
-import it.alexm.dndbot.logic.rollCharacteristic
-import it.alexm.dndbot.logic.rollSkill
-import it.alexm.dndbot.vo.Character
-import it.alexm.dndbot.vo.CharacteristicType
-import it.alexm.dndbot.vo.SkillType
 import java.io.File
 import java.nio.charset.Charset
 
@@ -19,33 +13,11 @@ suspend fun main() {
     val botKey = keyFile.readText(Charset.defaultCharset())
 
     bot(botKey) {
-        commands("!")  {
+        commands("!") {
             command("h") {
                 reply("Ciao Magaz questa è una prova!")
             }
         }
     }
-
-}
-
-fun main2() {
-    println("Prova")
-    val file = File("/Users/alex/.dndbot/DndPg.json")
-
-    val json = file.readText(Charset.defaultCharset())
-
-    file.exists()
-
-    val gson = Gson()
-
-    val c = gson.fromJson(json, Character::class.java)
-
-    println(c)
-    println()
-
-    println(c.rollCharacteristic(CharacteristicType.STR))
-
-    println(c.rollSkill(SkillType.ARCANA))
-    println(c.rollCharacteristic(CharacteristicType.STR, true))
 
 }
---
+++ src/test/java/it/alexm/dndbot/Test.kt
@@ -0,0 +1,48 @@
+package it.alexm.dndbot
+
+import com.google.gson.Gson
+import it.alexm.dndbot.logic.rollCharacteristic
+import it.alexm.dndbot.logic.rollSkill
+import it.alexm.dndbot.vo.Character
+import it.alexm.dndbot.vo.CharacteristicType
+import it.alexm.dndbot.vo.SkillType
+import org.junit.jupiter.api.Test
+import java.io.File
+import java.nio.charset.Charset
+
+
+class Test {
+
+    @Test
+    fun prova1() {
+        println("Prova")
+        val file = File("/Users/alex/.dndbot/pgs/DndPg.json")
+
+        val json = file.readText(Charset.defaultCharset())
+
+        file.exists()
+
+        val gson = Gson()
+
+        val c = gson.fromJson(json, Character::class.java)
+
+        println(c)
+        println()
+
+        println(c.rollCharacteristic(CharacteristicType.STR))
+
+        println(c.rollSkill(SkillType.ARCANA))
+        println(c.rollCharacteristic(CharacteristicType.STR, true))
+        println()
+    }
+
+
+    @Test
+    fun prova2() {
+        println("Lista di file dentro .dndbot/pgs:")
+        val pgs = File("/Users/alex/.dndbot/pgs/").list { _, file ->
+            file.contains(".json")
+        }
+        pgs?.forEach(::println)
+    }
+}
